d9e4bf145875f0276ebaf3131b8ee75212ac2ca6
orchifai <oscarfai121@gmail.com>
2018-11-18 16:54:43 +0800

    edit to View

--- app/src/main/java/com/orchifai/todoapp/MyAdapter.kt
+++ app/src/main/java/com/orchifai/todoapp/MyAdapter.kt
@@ -2,39 +2,51 @@ package com.orchifai.todoapp
 
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
 
-class MyAdapter(private val myDataset: Array<String>) :
+class MyAdapter(private val dataTemp: Array<String>) :
     RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+//        TODO: set a function to change the TextView inside the whole View
+    }
 
 
-    // Create new views (invoked by the layout manager)
+//     Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup,
                                     viewType: Int): MyAdapter.MyViewHolder {
         // create a new view
-        val textView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.my_text_view, parent, false) as TextView
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.my_text_view, parent, false) as View
         // set the view's size, margins, paddings and layout paramzeters
-        return MyViewHolder(textView)
+        return MyViewHolder(view)
     }
+
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+//        val layoutInflater = LayoutInflater.from(parent.context)
+//        val cellForRow = layoutInflater.inflate(R.layout.my_text_view, parent, false)
+//        return CustomViewHolder(cellForRow)
+//    }
 
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.text = myDataset[position]
+        holder.view.findViewById<TextView>(R.id.tvToDoItem).text = dataTemp[position]
     }
 
     // Return the size of your dataset (invoked by the layout manager)
 //    override fun getItemCount() = myDataset.size
-    override fun getItemCount() = 100
+    override fun getItemCount() = dataTemp.size
+
+}
+
+class CustomViewHolder(v: View): RecyclerView.ViewHolder(v){
 
 }
